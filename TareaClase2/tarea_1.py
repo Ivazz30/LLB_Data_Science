4b8ebe34dcd62e74de117153194acf402d91fa5d
Ivazz30 <ivannia.ramirez.maykall@gmail.com>
2019-05-13 20:05:40 -0600

    tarea_1

--- TareaClase2/tarea_1.py
+++ TareaClase2/tarea_1.py
@@ -1,6 +1,6 @@
 import math
 
-print "The value of PI is approximately: %.1s %0.7f " % ("******", math.pi)
+pi = ("******", math.pi)
 
+print "The value of PI is approximately: %.1s %0.7f" % pi + " %.2s %0.6f" % pi + " %.3s %0.5f" % pi + " %.4s %0.4f" % pi + " %.5s %0.3f" % pi + " %.6s %0.2f" % pi
 
-print "The value of PI is approximately: %.2s %0.6f " % ("******", math.pi)
